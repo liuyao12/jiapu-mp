09aba7e9d8f58b8938dcca435004747dbc83dcc5
Yao Liu <liuyao@gmail.com>
2026-06-23 15:05:30 +0800

    Remove duplicate pointer icons

--- pages/tree/tree.js
+++ pages/tree/tree.js
@@ -1013,25 +1013,8 @@ Page({
       if (node && node.id) counts[node.id] = (counts[node.id] || 0) + 1;
       return counts;
     }, {});
-    const primaryDuplicateById = (nodes || []).reduce((map, node) => {
-      if (node && node.id && !node.isDuplicatePlaceholder && idCounts[node.id] > 1 && !map[node.id]) {
-        map[node.id] = node;
-      }
-      return map;
-    }, {});
     return (nodes || []).map(node => {
-      const duplicatePrimary = node.isDuplicatePlaceholder ? primaryDuplicateById[node.id] : null;
-      const duplicatePointerDirection = duplicatePrimary
-        ? (Number(duplicatePrimary.y || 0) < Number(node.y || 0) ? 'up' : 'down')
-        : '';
-      const duplicatePointerSymbol = duplicatePointerDirection === 'up'
-        ? '☝'
-        : duplicatePointerDirection === 'down'
-          ? '☟'
-          : '';
-      const displayIconType = duplicatePointerDirection
-        ? `duplicate-${duplicatePointerDirection}`
-        : node.iconType;
+      const displayIconType = node.iconType;
       const nodeFillColor = node.gender === 'male'
         ? TREE_STYLE.maleFill
         : node.gender === 'female'
@@ -1062,8 +1045,6 @@ Page({
         ...node,
         iconType: displayIconType,
         originalIconType: node.iconType,
-        duplicatePointerDirection,
-        duplicatePointerSymbol,
         iconColor: node.iconType === 'marriage' ? this._getLineageToneColor(node.lineage) : TREE_STYLE.iconBorderColor,
         iconSrc: getTreeIconSrc(displayIconType, node.lineage),
         isDuplicateInstance: !!(node.id && idCounts[node.id] > 1),
@@ -7587,18 +7568,6 @@ Page({
       return;
     }
 
-    if (type === 'duplicate-up' || type === 'duplicate-down') {
-      const symbol = type === 'duplicate-up' ? '☝' : '☟';
-      const slotSize = style.iconSize;
-      ctx.save();
-      ctx.fillStyle = iconColor;
-      ctx.font = `bold ${Math.round(slotSize * 1.35)}px sans-serif`;
-      ctx.textAlign = 'center';
-      ctx.textBaseline = 'middle';
-      ctx.fillText(symbol, x + slotSize / 2, y + slotSize / 2);
-      ctx.restore();
-      return;
-    }
 
     const slotSize = style.iconSize;
     const iconSize = type === 'leaf' ? style.leafIconSize : style.iconSize;
